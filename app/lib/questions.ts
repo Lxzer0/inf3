af6ecc4058f91a54ee6584660940939b5507a9a0
Lxzer0 <dmytro.korczynski@gmail.com>
2025-04-29 08:32:13 +0200

    feat: fetch questions from database

---
+++ app/lib/questions.ts
@@ -0,0 +1,33 @@
+import 'server-only'
+import { db } from '@/app/lib/database'
+
+let cachedQuestions: Awaited<ReturnType<typeof fetchQuestions>> | null = null;
+
+async function fetchQuestions() {
+    return await db
+        .selectFrom('questions')
+        .select([
+            'question',
+            'answer_correct',
+            'answer_incorrect0',
+            'answer_incorrect1',
+            'answer_incorrect2',
+            'image',
+        ])
+        .execute();
+}
+
+export async function getQuestions(n: number) {
+    if (cachedQuestions) return shuffleArray(cachedQuestions).slice(0, n);
+    cachedQuestions = await fetchQuestions();
+    return shuffleArray(cachedQuestions).slice(0, n);
+}
+
+export function shuffleArray<T>(array: T[]): T[] {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+}
