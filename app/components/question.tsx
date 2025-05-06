0be806c5996729d1affb68699b7ccc4fd0122c5f
Erwin Grudowski <xerwinxpl@gmail.com>
2025-05-06 13:24:59 +0200

    feat: gamma UI, add keyboard navigation

--- app/components/question.tsx
+++ app/components/question.tsx
@@ -1,53 +1,121 @@
 "use client";
 import { newQuestionType } from "@/app/lib/questions";
 import { transformQuestions } from "@/app/lib/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function QuestionList({ questions }: { questions: newQuestionType[] }) {
-    const [cid, setCid] = useState<number>(0);
-    const [ongoing, setOngoing] = useState<boolean>(true);
-    const [nq, setNq] = useState<newQuestionType[]>(questions);
+    type data = {
+        currentQuestion: number;
+        ongoing: boolean;
+        questions: newQuestionType[];
+    };
+    const [data, setData] = useState<data>({
+        currentQuestion: 0,
+        ongoing: true,
+        questions,
+    });
 
     function handleClick(index: number) {
-        setNq((prev) => {
-            if (!ongoing) return prev;
-            const updated = [...prev];
-            updated[cid] = { ...updated[cid], selected: index };
-            return updated;
+        setData((prev) => {
+            if (prev.currentQuestion >= prev.questions.length) return prev;
+            const next = { ...prev };
+            if (next.ongoing) next.questions[next.currentQuestion].selected = index;
+            next.currentQuestion++;
+            return next;
         });
-        setCid((prev) => prev + 1);
     }
 
     function viewQuestions() {
-        setOngoing(false);
-        setCid(0);
+        setData((prev) => {
+            return {
+                currentQuestion: 0,
+                ongoing: false,
+                questions: prev.questions,
+            };
+        });
     }
 
     async function newQuestions() {
         try {
-            const response = await fetch('/api/questions');
-            if (!response.ok) {
-                throw new Error('Fetch failed');
-            }
-            const newQuestions: newQuestionType[] = transformQuestions(await response.json());
-            setNq(newQuestions);
-            setCid(0);
-            setOngoing(true);
+            const response = await fetch("/api/questions");
+            if (!response.ok) throw new Error("Fetch failed");
+            setData({
+                currentQuestion: 0,
+                ongoing: true,
+                questions: transformQuestions(
+                    await response.json(),
+                ) as newQuestionType[],
+            });
         } catch (error) {
             console.error(error);
         }
     }
 
+    function handleEvent(event: KeyboardEvent) {
+        if (event.altKey || event.ctrlKey || event.metaKey) return;
+        if (event.repeat) return;
+        switch (event.key) {
+            case "a":
+            case "f": {
+                handleClick(0);
+                break;
+            }
+            case "b":
+            case "g": {
+                handleClick(1);
+                break;
+            }
+            case "c":
+            case "h": {
+                handleClick(2);
+                break;
+            }
+            case "d":
+            case "j": {
+                handleClick(3);
+                break;
+            }
+            case "ArrowRight": {
+                setData((prev) => {
+                    const next = { ...prev };
+                    next.currentQuestion++;
+                    return next;
+                });
+                break;
+            }
+            case "ArrowLeft": {
+                setData((prev) => {
+                    const next = { ...prev };
+                    next.currentQuestion = next.currentQuestion < 1
+                        ? next.currentQuestion
+                        : next.currentQuestion - 1;
+                    return next;
+                });
+                break;
+            }
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleEvent);
+    }, []);
+
     return (
         <div className="w-full h-full flex items-center justify-center">
-            {cid < nq.length ? (
-                <Question el={nq[cid]} num={cid + 1} onSelect={handleClick} update={ongoing} />
+            {data.currentQuestion < data.questions.length ? (
+                <Question
+                    el={data.questions[data.currentQuestion]}
+                    num={data.currentQuestion + 1}
+                    onSelect={handleClick}
+                    update={data.ongoing}
+                />
             ) : (
                 <Last
-                    n={nq.length}
-                    correct={nq.reduce((acc, cur) => acc + (cur.selected == cur.correctI ? 1 : 0), 0)}
+                    n={data.questions.length}
+                    correct={data.questions.reduce((acc, cur) => acc + (cur.selected == cur.correctI ? 1 : 0), 0)}
                     viewQuestions={viewQuestions}
                     newQuestions={newQuestions}
+                    ongoing={data.ongoing}
                 />
             )}
         </div>
@@ -55,7 +123,8 @@ export function QuestionList({ questions }: { questions: newQuestionType[] }) {
 }
 
 function getClasses(i: number, el: newQuestionType, update: boolean): string {
-    if (update) return " hover:text-black hover:bg-white";
+    if (update && i == el.selected) return " bg-white text-black";
+    if (update) return " btn";
     if (i == el.correctI) return " bg-green-950";
     if (i == el.selected) return " bg-red-900";
     return "";
@@ -96,19 +165,22 @@ function Last({
     n,
     viewQuestions,
     newQuestions,
+    ongoing,
 }: {
     correct: number;
     n: number;
     viewQuestions: () => void;
     newQuestions: () => void;
+    ongoing: boolean;
 }) {
     return (
         <div className="border border-white text-xl p-4 flex max-w-2/3 items-center justify-center">
             <div className="w-full flex-col flex items-center justify-center gap-8 my-4">
-                <div>You have finished, {correct} correct ({100 * correct / n}%), {n - correct} wrong.</div>
+                <div>Click "Done" to show the stats and answers</div>
+                {ongoing ? null : <div>{correct} correct ({100 * correct / n}%), {n - correct} wrong.</div>}
                 <div className="flex flex-row gap-2">
                     <button className="border border-white cursor-pointer hover:text-black hover:bg-white p-2" onClick={newQuestions}>Try again</button>
-                    <button className="border border-white cursor-pointer hover:text-black hover:bg-white p-2" onClick={viewQuestions}>View questions</button>
+                    <button className="border border-white cursor-pointer hover:text-black hover:bg-white p-2" onClick={viewQuestions}>Done</button>
                 </div>
             </div>
         </div>
